18c2106b549cab5abe823d51dad8933e96614673
Chirag Tiwari <chiragtiwari2001@gmail.com>
2025-05-19 12:03:24 +0530

    Added active admin

---
+++ app/admin/event_details.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register EventDetail do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :event_date, :venue, :nomination_deadline
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:event_date, :venue, :nomination_deadline]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
--- app/models/event_detail.rb
+++ app/models/event_detail.rb
@@ -1,3 +1,9 @@
 class EventDetail < ApplicationRecord
   validates :event_date, :venue, :nomination_deadline, presence: true
+
+  private
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "event_date", "id", "id_value", "nomination_deadline", "updated_at", "venue"]
+  end
 end
